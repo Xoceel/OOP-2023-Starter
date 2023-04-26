6dfd189d2e0b43dc4e2e181e54610a73a59a42c9
Lee <echomartin12@gmail.com>
2023-04-26 17:24:25 +0100

    broken garbage

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -6,6 +6,10 @@ import processing.core.PApplet;
 
 public class DANI extends PApplet {
 
+	String[] splits;
+	String[] lines;
+	Word[] words;
+	ArrayList<Follow> fields;
 	
 
 	public void settings() {
@@ -27,7 +31,10 @@ public class DANI extends PApplet {
 	}
 
 	public void keyPressed() {
+		if(key == '0') {
+		loadFile();
 
+		}
 	}
 
 	float off = 0;
@@ -45,7 +52,38 @@ public class DANI extends PApplet {
 
 	// above is orginial methods
 	public void loadFile() {
-		
+		lines = loadStrings("small.txt"); // Load a text file into a String array]
+		fields = new ArrayList<Follow>();
+		for (int i = 0; i < lines.length; i++) {
+			lines[i] = lines[i].replaceAll("[^\\w\\s]",""); // Remove punction characters
+			lines[i] = lines[i].toLowerCase(); // Convert a string to lower case 
+			splits = split(lines[i], ' '); // Split a string into an array of words
+			for(int j = 0; j < splits.length; j ++) {
+
+				while(!findWord(splits[j])) {
+					fields.get(i).word = splits[j+1];
+					fields.get(i).count++;
+
+					words[j] = new Word(splits[j], fields);
+				}
+			}
+
+		}
+	}
+
+
+	public void printModel() {
+		for(int i = 0; i < splits.length; i++) {
+			System.out.println(splits[i]);
+		}
+	}
+	public boolean findWord(String str) {
+		for (int i = 0; i < words.length; i++) {
+			if (str.equals(words[i].word)){
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
---
+++ java/src/ie/tudublin/Word.java
@@ -0,0 +1,34 @@
+package ie.tudublin;
+
+import java.util.ArrayList;
+
+public class Word {
+    String word;
+    ArrayList<Follow> fields;
+
+    public Word(String word, ArrayList<Follow> fields) {
+        this.word = word;
+        this.fields = fields;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public ArrayList<Follow> getfields() {
+        return fields;
+    }
+
+    public void setfields(ArrayList<Follow> fields) {
+        this.fields = fields;
+    }
+
+    // @Override
+    // public String toString() {
+    //     continue
+    // }
+}
